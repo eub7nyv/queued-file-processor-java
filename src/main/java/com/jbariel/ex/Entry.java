7da001991955508be1bea3136c0607415ac2f2fa
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-01-15 16:09:37 -0600

    support switch statements (general); specifically support taking a filename, then loading the file; support exit status for cause of exit

--- src/main/java/com/jbariel/ex/Entry.java
+++ src/main/java/com/jbariel/ex/Entry.java
@@ -18,10 +18,141 @@
  */
 package com.jbariel.ex;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Entry {
 
+	public enum ExitStatus {
+		NORMAL, HELP_SHOWN, UNKNOWN, FILE_NOT_FOUND, FILE_NOT_CLOSED_CORRECTLY,
+	}
+
+	private static Logger log = LoggerFactory.getLogger(Entry.class);
+
+	private static EntryManager mgr = EntryManager.instance();
+
 	public static void main(String[] args) {
-		// main entry point
+		mgr.readArgs(args);
+
+		// ACTUALLY DO STUFF
+
+		mgr.closeFileIfOpened();
+		exit(ExitStatus.NORMAL);
+	}
+
+	public static void exit(ExitStatus status) {
+		System.exit((status == null) ? ExitStatus.UNKNOWN.ordinal() : status.ordinal());
+	}
+
+	public static BufferedReader getFileToParse() {
+		return mgr.getFile();
+	}
+
+	static class EntryManager {
+
+		private static final EntryManager entryManager = new EntryManager();
+
+		protected static EntryManager instance() {
+			return entryManager;
+		}
+
+		private EntryManager() {
+			// hide the default constructor
+			super();
+		}
+
+		private String filename;
+
+		private BufferedReader file;
+
+		private String getFilename() {
+			return this.filename;
+		}
+
+		private void setFilename(String filename) {
+			this.filename = filename;
+			tryToLoadFile();
+		}
+
+		protected BufferedReader getFile() {
+			return this.file;
+		}
+
+		protected void tryToLoadFile() {
+			try {
+				file = new BufferedReader(new FileReader(getFilename()));
+			} catch (FileNotFoundException e) {
+				log.error("Could not find file with filename: '" + getFilename()
+						+ "'!  Check your input and try again.\n");
+				printHelpAndExit(ExitStatus.FILE_NOT_FOUND);
+			}
+			log.info("Successfully loaded file with filename: '" + getFilename() + "'");
+		}
+
+		private void readArgs(String[] args) {
+			if (args.length == 0) {
+				printHelpAndExit();
+			}
+			int index = 0;
+			String tmpFlag, tmpVal;
+			do {
+				tmpFlag = StringUtils.trimToEmpty(args[index++]);
+				tmpVal = (index < (args.length)) ? StringUtils.trimToEmpty(args[index++]) : StringUtils.EMPTY;
+
+				switch (tmpFlag) {
+				case "-f":
+					setFilename(tmpVal);
+					break;
+				case "-?":
+				case "-H":
+				case "-h":
+					printHelpAndExit();
+					break;
+				default:
+					log.error("Unknown flag: '" + tmpFlag + "'!!!");
+					printHelpAndExit();
+				}
+
+			} while (index < args.length);
+
+		}
+
+		protected void closeFileIfOpened() {
+			if (null != file) {
+				try {
+					file.close();
+					log.info("Successfully closed file: '" + getFilename() + "'");
+				} catch (IOException e) {
+					log.error("Failed to close file: '" + getFilename() + "'!!!");
+					e.printStackTrace();
+					Entry.exit(ExitStatus.FILE_NOT_CLOSED_CORRECTLY);
+				}
+			}
+		}
+
+		private void printHelpAndExit() {
+			printHelpAndExit(ExitStatus.HELP_SHOWN);
+		}
+
+		private void printHelpAndExit(ExitStatus status) {
+			System.out.println(
+					"\n===================================== HOW TO USE THIS JAR ====================================="
+							+ "\n\tThis jar can be called with switched arguments as follows:"
+							+ "\n\n\t\t -f <filename>   => sets the file to process"
+							+ "\n\t\t\t THIS FILE MUST BE RELATIVE TO THE CWD - NOT THE JAR FILE"
+							+ "\n\n\t\t -h | -H | -?    => prints this help and exits"
+							+ "\n\n===============================================================================================\n\n");
+
+			closeFileIfOpened();
+			Entry.exit(status);
+		}
+
 	}
 
 }
